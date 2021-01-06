5a26bc8d363a084bc6bc268bcfa1a44a97d08250
uPagge <upagge@mail.ru>
2021-01-06 22:22:56 +0300

    contact

--- src/main/java/org/sadtech/example/swagger/config/SwaggerConfig.java
+++ src/main/java/org/sadtech/example/swagger/config/SwaggerConfig.java
@@ -1,12 +1,12 @@
 package org.sadtech.example.swagger.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- *
  * @author upagge 30.12.2020
  */
 @Configuration
@@ -19,6 +19,12 @@ public class SwaggerConfig {
                         new Info()
                                 .title("Loyalty System Api")
                                 .version("1.0.0")
+                                .contact(
+                                        new Contact()
+                                                .email("me@upagge.ru")
+                                                .url("https://uPagge.ru")
+                                                .name("Struchkov Mark")
+                                )
                 );
     }
 
